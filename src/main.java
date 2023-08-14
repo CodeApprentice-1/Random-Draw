4c894b2ce117aa7ce16210fd89b1367090dbed60
CodeApprentice-1 <92059587+CodeApprentice-1@users.noreply.github.com>
2023-08-14 19:10:00 +0100

    Update main.java
    Clean code

--- src/main.java
+++ src/main.java
@@ -1,21 +1,16 @@
 
 public class main {
-
 	public static void main (String args[]) {
 		int n=10;
 		System.out.println(fib(n));
 	}
-	
 	public static int fib(int n) {
 		int fib[]=new int[n+2];
-		
 		fib[0]=0;
 		fib[1]=1;
-		
 		for (int i=2;i<=n; i++) {
 			fib[i]=fib[i-1]+fib[i-2];
 		}
-		
 		return fib[n];
 	}
 }
